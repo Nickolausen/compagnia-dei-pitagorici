26480f4b1fe42e485c3d13e38cd22e9baa07cfad
Nicholas Magi <nicholas.magi24@gmail.com>
2024-09-05 16:49:37 +0200

    aggiunto file workflow

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,8 +4,8 @@ import styles from './Home.module.css'
 function Home() 
 {     
     return (
-        <section className='d-flex justify-content-center align-items-center flex-column'>
-            <img className={styles.main_tetraktys + ' w-75 w-md-50 exclude'} src='src\assets\imgs\tetraktys_nowriting.png'></img>
+        <section className={styles.main_tetraktys + ' d-flex justify-content-center align-items-center flex-column'}>
+            <img className={'w-75 w-md-50 exclude'} src='src\assets\imgs\tetraktys_nowriting.png'></img>
             <h1 className={styles.main_title + ' pt-4'}>Compagnia dei Pitagorici</h1>
             <p className='fs-1 pt-2 lh-sm'>
                 Errare humanum est, perseverare... <span className='fw-bold'>pytagoricum</span>!
