08492c190e7db941d2fe7e4493c0523686a77ffb
Nickolausen <nicholas.magi24@gmail.com>
2024-10-13 15:09:26 +0200

    assets fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ function Home()
 {     
     return (
         <section className={styles.main_container + ' d-flex justify-content-center align-items-center flex-column'}>
-            <img className={styles.main_tetraktys + ' w-75 w-md-50 exclude'} src={import.meta.env.BASE_URL + 'imgs\\tetraktys_nowriting.png'}></img>
+            <img className={styles.main_tetraktys + ' w-75 w-md-50 exclude'} src={import.meta.env.BASE_URL + '\\imgs\\tetraktys_nowriting.png'}></img>
             <h1 className={styles.main_title + ' pt-4'}>Compagnia dei Pitagorici</h1>
             <p className='fs-1 pt-2 lh-sm px-3 px-md-0'>
                 Errare humanum est, perseverare... <span className='fw-bold'>pytagoricum</span>!
